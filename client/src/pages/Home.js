ac212a9e33df1fbf0171464711056b60a903c740
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-03-27 19:43:14 -0400

    Home page set up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import InputChatGPT from "../components/InputChatGPT";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      Home
+      <InputChatGPT />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
