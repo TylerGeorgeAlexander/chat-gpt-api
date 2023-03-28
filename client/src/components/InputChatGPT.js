3e19cbbc840371f58ed80b8ff7239de6b66e3c95
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-03-27 20:33:24 -0400

    feature: displays using react-markdown

--- client/src/components/InputChatGPT.js
+++ client/src/components/InputChatGPT.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 const InputChatGPT = () => {
   const [input, setInput] = useState("");
@@ -30,10 +31,25 @@ const InputChatGPT = () => {
   };
 
   return (
-    <div>
-      <textarea value={input} onChange={handleChange} />
-      <button onClick={handleClick}>Send</button>
-      <div>{output}</div>
+    <div className="flex flex-col gap-2 p-4">
+      <label htmlFor="chat-prompt" className="font-bold">
+        Input chatGPT prompt(s):
+      </label>
+      <textarea
+        id="chat-prompt"
+        className="border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+        value={input}
+        onChange={handleChange}
+      />
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        onClick={handleClick}
+      >
+        Send
+      </button>
+      <div className="prose">
+        <ReactMarkdown>{`${output}`}</ReactMarkdown>
+      </div>
     </div>
   );
 };
