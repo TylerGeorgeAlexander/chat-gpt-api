c7d446b70ce7a28f2c258e0fe6d60d66777027a3
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-03-27 20:43:30 -0400

    feature: concurrently script added

--- client/src/components/InputChatGPT.js
+++ client/src/components/InputChatGPT.js
@@ -45,7 +45,7 @@ const InputChatGPT = () => {
         className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
         onClick={handleClick}
       >
-        Send
+        Generate
       </button>
       <div className="prose">
         <ReactMarkdown>{`${output}`}</ReactMarkdown>
