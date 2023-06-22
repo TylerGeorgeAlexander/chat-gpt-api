2f7ad778ebe189689d54a41bd10c9893782a53f8
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-06-22 07:56:15 -0400

    Refactor user registration and login controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,7 @@ const registerUser = async (req, res) => {
         // Check if user with the same email already exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: 'User with this email already exists' });
+            return res.status(400).json({ error: 'User with this email already exists' });
         }
 
         // Hash the password
@@ -26,61 +26,58 @@ const registerUser = async (req, res) => {
         // Save the user to the database
         await newUser.save();
 
-        // Redirect to the login page
-        res.redirect('/login');
+        // Return success response
+        res.status(200).json({ message: 'User registration successful' });
     } catch (error) {
         console.error('Error in registering user:', error);
-        res.status(500).json({ message: 'An error occurred during user registration' });
+        res.status(500).json({ error: 'An error occurred during user registration' });
     }
 };
-
 
 // Controller function for user login
 const login = async (req, res) => {
     try {
-      const { email, password } = req.body;
-  
-      // Find the user by email
-      const user = await User.findOne({ email });
-  
-      // User not found
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-  
-      // Compare passwords
-      const isMatch = await bcrypt.compare(password, user.password);
-  
-      // Password does not match
-      if (!isMatch) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-      }
-  
-      // Password matches, user authenticated
-      // Generate a JWT token
-      const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: '1h', // Set the token expiration time as desired
-      });
-  
-      // Send the authToken to the frontend
-      return res.status(200).json({ message: 'Login successful', authToken });
+        const { email, password } = req.body;
+
+        // Find the user by email
+        const user = await User.findOne({ email });
+
+        // User not found
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        // Password does not match
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        // Password matches, user authenticated
+        // Generate a JWT token
+        const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1h', // Set the token expiration time as desired
+        });
+
+        // Send the authToken to the frontend
+        res.status(200).json({ message: 'Login successful', authToken });
     } catch (err) {
-      console.error('Error in logging in:', err);
-      return res.status(500).json({ message: 'An error occurred' });
+        console.error('Error in logging in:', err);
+        res.status(500).json({ error: 'An error occurred during login' });
     }
-  };
+};
 
 // Controller function for user logout
 const logout = (req, res) => {
     req.logout(); // Logout the user using Passport.js
-  
+
     // Clear the authToken stored in localStorage
     localStorage.removeItem('authToken');
-  
-    res.status(200).json({ message: 'Logout successful' });
-  };
-  
 
+    res.status(200).json({ message: 'Logout successful' });
+};
 
 // Controller function to get user profile
 const getUserProfile = (req, res) => {
@@ -90,16 +87,16 @@ const getUserProfile = (req, res) => {
     User.findById(userId, (err, user) => {
         if (err) {
             console.error(err);
-            return res.status(500).json({ message: 'An error occurred' });
+            return res.status(500).json({ error: 'An error occurred while fetching user profile' });
         }
 
         // User not found
         if (!user) {
-            return res.status(404).json({ message: 'User not found' });
+            return res.status(404).json({ error: 'User not found' });
         }
 
         // Return the user profile
-        return res.status(200).json({ user });
+        res.status(200).json({ user });
     });
 };
 
