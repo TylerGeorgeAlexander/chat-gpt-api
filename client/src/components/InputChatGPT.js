b2947d4d04f39eca1f6785c975849e4cc3569736
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-06-26 19:56:50 -0400

    Update styling for icons in search history component

--- client/src/components/InputChatGPT.js
+++ client/src/components/InputChatGPT.js
@@ -184,7 +184,7 @@ const InputChatGPT = () => {
                 {showConfirmation === index ? (
                   <>
                     <button
-                      className="text-red-500 hover:text-red-700 ml-2"
+                      className="text-gray-500 hover:text-gray-700 ml-2"
                       onClick={() => deleteUserSearchHistory(search._id)}
                     >
                       <FiCheck size={16} />
@@ -199,7 +199,7 @@ const InputChatGPT = () => {
                 ) : (
                   <>
                     <button
-                      className="text-red-500 hover:text-red-700 ml-2"
+                      className="text-gray-500 hover:text-gray-700 ml-2"
                       onClick={() => setShowConfirmation(index)}
                     >
                       <FiTrash2 size={16} />
