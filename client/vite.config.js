436a856b7d0b88c496a83701e790c1407a40115a
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-11-06 19:18:44 -0500

    feat: utilizing dotenv in conjuntion with replace to properly import our VITE_REACT_APP_BASE_URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import envPlugin from 'vite-plugin-env';
+import replace from '@rollup/plugin-replace';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+// Load environment variables from .env file
+dotenv.config();
+
 export default defineConfig({
   plugins: [
     react(),
-    envPlugin(),
+    replace({
+      'process.env.REACT_APP_BASE_URL': JSON.stringify(process.env.REACT_APP_BASE_URL),
+    }),
   ],
 });
