1c8705159fb3a18a31f6323416d59837288fe1da
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-11-15 08:29:36 -0500

    feat: derive flashcard style set in localStorage with Settings page else oneLight default

--- client/src/components/FlashCard.jsx
+++ client/src/components/FlashCard.jsx
@@ -1,5 +1,5 @@
 // FlashCard.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import * as PrismStyles from "react-syntax-highlighter/dist/esm/styles/prism";
@@ -11,73 +11,19 @@ const getStyleObject = (styleName) => {
 const FlashCard = ({ title, query, assertion, timestamp }) => {
   const [selectedStyle, setSelectedStyle] = useState("oneLight");
 
-  const handleStyleChange = (e) => {
-    setSelectedStyle(e.target.value);
-  };
-
-  const styles = [
-    "coy",
-    "dark",
-    "funky",
-    "okaidia",
-    "solarizedlight",
-    "tomorrow",
-    "twilight",
-    "prism",
-    "a11yDark",
-    "atomDark",
-    "base16AteliersulphurpoolLight",
-    "cb",
-    "coldarkCold",
-    "coldarkDark",
-    "coyWithoutShadows",
-    "darcula",
-    "dracula",
-    "duotoneDark",
-    "duotoneEarth",
-    "duotoneForest",
-    "duotoneLight",
-    "duotoneSea",
-    "duotoneSpace",
-    "ghcolors",
-    "gruvboxDark",
-    "gruvboxLight",
-    "holiTheme",
-    "hopscotch",
-    "lucario",
-    "materialDark",
-    "materialLight",
-    "materialOceanic",
-    "nightOwl",
-    "nord",
-    "oneDark",
-    "oneLight",
-    "pojoaque",
-    "shadesOfPurple",
-    "solarizedDarkAtom",
-    "synthwave84",
-    "vs",
-    "vscDarkPlus",
-    "xonokai",
-    "zTouch",
-  ];
+  useEffect(() => {
+    // Get the selected style from local storage
+    const savedStyle = localStorage.getItem("selectedStyle");
+    if (savedStyle) {
+      setSelectedStyle(savedStyle);
+    } else {
+      // If there isn't a style in local storage, set the default style to "oneLight"
+      setSelectedStyle("oneLight");
+    }
+  }, []);
 
   return (
     <div className="max-h-[calc(100vh-7.5rem)] overflow-y-auto pr-4">
-      <div>
-        <label htmlFor="styleSelect">Select a style:</label>
-        <select
-          id="styleSelect"
-          value={selectedStyle}
-          onChange={handleStyleChange}
-        >
-          {styles.map((style) => (
-            <option key={style} value={style}>
-              {style}
-            </option>
-          ))}
-        </select>
-      </div>
       <div className="bg-white shadow-md rounded p-4 m-4">
         <h2 className="font-bold text-lg">{title}</h2>
         <p className="text-gray-600 text-xs">Query: {query}</p>
